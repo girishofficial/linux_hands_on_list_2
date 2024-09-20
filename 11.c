c151425a6f260f3d161cd01fb7daabf9983d0151
Girish Kumar Sahu <81010285+girishofficial@users.noreply.github.com>
2024-09-21 01:21:47 +0530

    11.c added

--- 11.c
+++ 11.c
@@ -1,54 +1,41 @@
 /*
 ============================================================================
-Name : 11.c
-Author : Girish Kumar Sahu
-Description : Write a program to ignore a SIGINT signal then reset the default action of the SIGINT signal -
-use sigaction system call.
-signal - Use signal system call. 
-Date: 19th Sep, 2024.
+Name        : 11.c
+Author      : GIRISH KUMAR SAHU
+Description : Write a program to ignore SIGINT signal then reset the default 
+              action of SIGINT using sigaction system call.
+Date        : 20th Sep, 2024
 ============================================================================
 */
-#include<stdio.h>
-#include<stdlib.h>
-#include<signal.h>
-#include<unistd.h>
 
-void handler(int signalNo) {
-	if(signalNo == SIGFPE){
-		printf("Caught SIGFE \n");
-		exit(1);
-	}
-	else if(signalNo == SIGINT){
-		printf("Caught SIGINT (Ctrl+C)\n");
-		exit(1);
-	}
-	else if(signalNo == SIGSEGV){
-		printf("Caught SIGSEGV (segmentation fault)\n");
-		exit(1);
-	}
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handler(int sig) {
+    printf("SIGINT ignored...\n");
 }
 
-int main(){
-	struct sigaction act;
+int main() {
+    struct sigaction sa;
 
-	act.sa_handler = handler;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0;
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
 
-	sigaction(SIGFPE,&act,NULL);
-	sigaction(SIGINT,&act,NULL);
-	sigaction(SIGSEGV,&act,NULL);
+    sigaction(SIGINT, &sa, NULL);
 
-	while(1){
-	//	printf("press ctrl+c for sigint\n ");
-	//	sleep(1);
-	
-	/*int x = 5/0; this is to get SIGFPE trigger*/
-	int *ptr = NULL;
-	*ptr = 42; /*this will give SIGPE*/
-	}
-	return 0;
+    printf("Ignoring SIGINT for 5 seconds. Try pressing Ctrl+C...\n");
+    sleep(5);
+
+    printf("Resetting SIGINT to default action.\n");
+    sa.sa_handler = SIG_DFL; 
+    sigaction(SIGINT, &sa, NULL);
+
+    sleep(5);
+    printf("Program ends, now Ctrl+C works as usual.\n");
+
+    return 0;
 }
 
-/*output 
- Caught SIGSEGV*/
